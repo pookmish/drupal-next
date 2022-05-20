1b6989b858e0f591f8dd8c45b6501473c3890ee9
Mike Decker <pookmish@gmail.com>
2022-05-20 15:02:10 -0700

    banner behavior

--- components/paragraphs/banner.tsx
+++ components/paragraphs/banner.tsx
@@ -1,7 +1,7 @@
 import {DrupalImage} from "@/components/simple/image";
 import {Card} from "@/components/paragraphs/card";
 
-export const Banner = ({su_banner_body, su_banner_button, su_banner_header, su_banner_image, su_banner_sup_header}) => {
+export const Banner = ({su_banner_body, su_banner_button, su_banner_header, su_banner_image, su_banner_sup_header, behavior_settings}) => {
   const imageUrl = su_banner_image?.field_media_image?.image_style_uri?.breakpoint_2xl_2x;
   const hasCardText = su_banner_button || su_banner_header || su_banner_sup_header || su_banner_body;
   return (
@@ -20,7 +20,7 @@ export const Banner = ({su_banner_body, su_banner_button, su_banner_header, su_b
 
       {hasCardText &&
           <div
-              className="card su-block su-max-w-600 su-basefont-23 su-leading-display su-bg-white su-text-black su-border su-border-solid su-border-black-10 su-shadow su-relative su-max-w-full md:su-max-w-600 md:su-absolute md:su-top-auto md:su-left-36 md:su-bottom-36">
+              className={"card su-block su-max-w-600 su-basefont-23 su-leading-display su-bg-white su-text-black su-border su-border-solid su-border-black-10 su-shadow su-relative su-max-w-full md:su-max-w-600 md:su-absolute md:su-top-auto md:su-bottom-36 " + (behavior_settings?.hero_pattern?.overlay_position === 'right' ? 'md:su-right-36' : 'md:su-left-36')}>
               <Card
                   su_card_body={su_banner_body}
                   su_card_header={su_banner_header}
