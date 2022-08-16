eca53f6ff2539e3338099b82e35024fdca63d8aa
Mike Decker <pookmish@gmail.com>
2022-08-15 22:11:06 -0700

    switch between list and card displays for view results

--- components/paragraphs/stanford-lists.tsx
+++ components/paragraphs/stanford-lists.tsx
@@ -3,7 +3,8 @@ import useSWR from 'swr';
 import {ListParagraph} from "../../types/drupal";
 import formatHtml from "@/lib/format-html";
 import {DrupalLinkButton} from "@/components/simple/link";
-import {NodeCardDisplay} from "@/nodes/index";
+import {NodeCardDisplay, NodeListDisplay} from "@/nodes/index";
+import {DrupalNode} from "next-drupal";
 
 interface ListProps {
   paragraph: ListParagraph
@@ -35,9 +36,14 @@ export const StanfordLists = ({paragraph, siblingCount, ...props}: ListProps) =>
       {paragraph.su_list_headline && <h2 className={`su-text-center`}>{paragraph.su_list_headline}</h2>}
       {paragraph.su_list_description && <div>{formatHtml(paragraph.su_list_description.processed)}</div>}
 
-      <div className={`lg:su-grid ${gridClass}`}>
+      <div className={`lg:su-grid su-gap-[50px] ${gridClass}`}>
         {itemsToDisplay?.map(item => (
-          <NodeCardDisplay key={item.id} node={item}/>
+          <ListItem
+            key={item.id}
+            node={item}
+            viewId={paragraph.su_list_view.resourceIdObjMeta.drupal_internal__target_id}
+            viewDisplayId={paragraph.su_list_view.resourceIdObjMeta.display_id}
+          />
         ))}
       </div>
 
@@ -48,4 +54,32 @@ export const StanfordLists = ({paragraph, siblingCount, ...props}: ListProps) =>
     </div>
   )
 
+}
+
+interface ListItemProps {
+  node: DrupalNode
+  viewId: string
+  viewDisplayId: string
+}
+
+const ListItem = ({node, viewId, viewDisplayId}: ListItemProps) => {
+  const display = `${viewId}:${viewDisplayId}`
+
+  switch (display) {
+    case 'stanford_events:cards':
+    case 'stanford_news:vertical_cards':
+    case 'stanford_person:grid_list_all':
+    case 'stanford_events:past_events_list_block':
+    case 'stanford_shared_tags:card_grid':
+    case 'stanford_basic_pages:viewfield_block_1':
+    case 'stanford_courses:vertical_teaser_viewfield_block':
+      return <NodeCardDisplay node={node}/>
+    case 'stanford_events:list_page':
+    case 'stanford_news:block_1':
+    case 'stanford_publications:chicago_list':
+    case 'stanford_publications:apa_list':
+    case 'stanford_basic_pages:basic_page_type_list':
+    case 'stanford_courses:default_list_viewfield_block':
+      return <NodeListDisplay node={node}/>
+  }
 }
